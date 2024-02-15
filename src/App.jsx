22a97b34b24ab4b856afe4529b73e656a9b51e68
Yahini <136244447+Yahinisekar@users.noreply.github.com>
2024-02-15 21:56:55 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -84,7 +84,7 @@ const App = () => {
       comment: 'No comments'
     },{
       id: 9,
-      img: 'https://www.guvi.in/blog/wp-content/uploads/2022/06/Types-of-Cyber-Security-Attacks-and-How-to-Minimize-the-Impact.jpg',
+      img: 'https://www.guvi.in/blog/wp-content/uploads/2020/11/cyber-security-e1614342903477.png',
       field: 'cybersecurity',
       title: 'Cybersecurity Vs Ethical Hacking: Top 10 Differences',
       desc: 'Cybersecurity & Ethical hacking and have been key in making sure that your data online',
